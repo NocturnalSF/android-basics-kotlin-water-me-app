42f0fb4a37d0dd7b901384fa93dd3a49dcb47fb1
gamer5459 <rocker5459@yahoo.com>
2022-07-10 14:26:34 -0400

    Updating Dependencies Breaks Code Finished Project

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,31 +18,42 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
-class PlantViewModel(application: Application): ViewModel() {
+class PlantViewModel(val application: Application): ViewModel() {
 
     val plants = DataSource.plants
-
     internal fun scheduleReminder(
         duration: Long,
         unit: TimeUnit,
         plantName: String
     ) {
         // TODO: create a Data instance with the plantName passed to it
-
+        val data = Data.Builder()
+            .putString(WaterReminderWorker.nameKey,
+                plantName)
+            .build()
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
-
+        val reminder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInitialDelay(duration, unit)
+            .setInputData(data)
+            .build()
         // TODO: Enqueue the request as a unique work request
+        WorkManager.getInstance(application)
+            .enqueueUniqueWork(plantName,
+            ExistingWorkPolicy.REPLACE,
+            reminder)
     }
 }
 
 class PlantViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(PlantViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
             PlantViewModel(application) as T
         } else {
             throw IllegalArgumentException("Unknown ViewModel class")
